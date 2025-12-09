4b6c4a91216a63c9c838a1974396ba7540a3416a
Andrew <126329757+Andrey-Russia@users.noreply.github.com>
2025-12-09 09:43:49 +0300

    a

---
+++ Assets/jump.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            anim.Play("jump");
+    }
+}
